200f96df6dfc828fe1a3de835ee877b1e5b50281
JoeyGinorio <joeyvelez@knights.ucf.edu>
2020-05-29 07:18:29 -0400

    removed import of State monad, since ended up not using it for eval

--- SysF.hs
+++ SysF.hs
@@ -5,7 +5,6 @@ Defines syntax, typing, evaluation for System F. Follows the specification in
 Chapter 23 of Pierce's Types and Programming Languages.
 -}
 
-import Control.Monad.State
 import Data.Map as Map
 
 {- ====================== Syntax of Terms & Types  ==========================-}
