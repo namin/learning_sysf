847cc3f908f72d61fb22399e66d69e5c917276eb
JoeyGinorio <joeyvelez@knights.ucf.edu>
2020-06-16 21:54:18 -0400

    edited the topline comment

--- SysF.hs
+++ SysF.hs
@@ -1,8 +1,8 @@
 {-|
 SysF.hs
-=========
+==============================================================================
 Defines syntax, typing, evaluation for System F. Follows the specification in
-Chapter 23 of Pierce's Types and Programming Languages.
+Chapter 2 of "Learning in System F".
 -}
 
 module SysF where
